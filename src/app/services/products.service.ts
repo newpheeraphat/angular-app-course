f8cc3aaba2cfe2e6d3942333201a8819e9af8cc8
Pheeraphat Prisan <77052746+newpheeraphat@users.noreply.github.com>
2024-04-04 23:51:17 +0700

    feat: Adding style of homepage

---
+++ src/app/services/products.service.ts
@@ -0,0 +1,21 @@
+import { Observable } from 'rxjs';
+import { ApiService } from './api.service';
+import { Injectable } from '@angular/core';
+import { PaginationParams, Products } from '../../types';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductsService {
+  constructor(private apiService: ApiService) {}
+
+  getProducts = (
+    url: string,
+    params: PaginationParams
+  ): Observable<Products> => {
+    return this.apiService.get(url, {
+      params,
+      responseType: 'json',
+    });
+  };
+}
